c319da10bd1745653bc70daacabbd3a69435f008
Beforg <bruno.capsqn@gmail.com>
2026-09-15 15:30:43 -0300

    utilizando switch-case

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+let dia = 3;
+
+switch (dia) {
+    case 1:
+        console.log("Segunda-Feira");
+        break;
+    case 2:
+        console.log("Terça-Feira");
+        break;
+    case 3:
+        console.log("Quarta-Feira");
+        break;
+    case 4:
+        console.log("Quinta-Feira");
+        break;
+    case 5:
+        console.log("Sexta-Feira");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+    case 7:
+        console.log("Domingo");
+        break;
+    default:
+        console.log("Dia inválido");
+}
