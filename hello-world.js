0f1681382ec1150101a8934a30403627359586d9
Gustavo Savi <gutosavi@hotmail.com>
2026-09-15 15:41:30 -0300

    feat: adiciona função para mapear os dias da semana

--- hello-world.js
+++ hello-world.js
@@ -1,2 +1,22 @@
-console.log("Hello World")
+const checkDayWeek = (day) => {
+  switch (day) {
+    case 0:
+      return "Domingo";
+    case 1:
+      return "Segunda-feira";
+    case 2:
+      return "Terça-feira";
+    case 3:
+      return "Quarta-feira";
+    case 4:
+      return "Quinta-feira";
+    case 5:
+      return "Sexta-feira";
+    case 6:
+      return "Sábado";
+    default:
+      return "Dia inválido";
+  }
+};
 
+console.log(checkDayWeek(0));
