13b711e3b56d32765790b27c0f7c78bf29eb8b0b
richGuilherme <richardbold.rg@gmail.com>
2026-09-15 15:36:08 -0300

    feat: implementação dos dias das semanas

---
+++ dias-da-semana.js
@@ -0,0 +1,28 @@
+const dia = 5;
+
+switch (dia) {
+  case 1:
+    console.log("Segunda-feira");
+    break;
+  case 2:
+    console.log("Terça-feira");
+    break;
+  case 3:
+    console.log("Quarta-feira");
+    break;
+  case 4:
+    console.log("Quinta-feira ");
+    break;
+  case 5:
+    console.log("Sexta-feira");
+    break;
+  case 6:
+    console.log("Sábado");
+    break;
+  case 7:
+    console.log("Domingo");
+    break;
+  default:
+    console.log("Dia inválido");
+    break;
+}
