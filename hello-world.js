68da871f738e72b29b07039a17e1c86c6e576fb5
richGuilherme <richardbold.rg@gmail.com>
2026-09-15 15:28:16 -0300

    feat: add bloco de switch para dias da semana

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+console.log("Hello World");
+
+const dia = 5;
+
+switch (dia) {
+  case 1:
+    console.log("Segunda-feira");
+    break;
+  case 2:
+    console.log("Terça-feira");
+    break;
+  case 3:
+    console.log("Quarta-feira");
+    break;
+  case 4:
+    console.log("Quinta-feira ");
+    break;
+  case 5:
+    console.log("Sexta-feira");
+    break;
+  case 6:
+    console.log("Sábado");
+    break;
+  case 7:
+    console.log("Domingo");
+    break;
+  default:
+    console.log("Dia inválido");
+    break;
+}
