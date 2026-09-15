29958c226729f0343974c89a88beb88afd57e5b3
Joao Lucas Monteiro <joaopb187@hotmail.com>
2026-09-15 15:31:53 -0300

    feature: Adicionado Switch Case

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+let diasemana = new Date().getDay()
+
+switch(diasemana){
+    case 1:
+        console.log("Domingo")
+        break
+    case 2:
+        console.log("Segunda")
+        break
+    case 3:
+        console.log("Terça")
+        break
+    case 4:
+        console.log("Quarta")
+        break
+    case 5:
+        console.log("Quinta")
+        break
+    case 6:
+        console.log("Sexta")
+        break
+    case 7:
+        console.log("Sábado")
+        break
+    default:
+        console.log("Invalido")
+}
