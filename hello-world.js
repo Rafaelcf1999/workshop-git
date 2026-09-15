9d6540956fcb5d306be8dfd903bb6ae13a9b3cd3
G-Deon <guilhermedeon29@gmail.com>
2026-09-15 16:30:59 -0300

    feat: implementação de exemplos de laços de repetição e tomadas de decisão

--- hello-world.js
+++ hello-world.js
@@ -1,27 +1,86 @@
-const dia = 0;
+const dia = 4;
 
 switch (dia) {
   case 1:
     console.log("Domingo");
     break;
   case 2:
-    console.log("Segunda");
+    console.log("Segunda-Feira");
     break;
   case 3:
-    console.log("Terça");
+    console.log("Terça-Feira");
     break;
   case 4:
-    console.log("Quarta");
+    console.log("Quarta-Feira");
     break;
   case 5:
-    console.log("Quinta");
+    console.log("Quinta-Feira");
     break;
   case 6:
-    console.log("Sexta");
+    console.log("Sexta-Feira");
     break;
   case 7:
-    console.log("Sábado");
+    console.log("Sábado-Feira");
     break;
   default:
     console.log("Valor inválido! (somente 1-7 são válidos)");
 }
+
+console.log("\n");
+
+///////////////////////////////////////////////////////////////////////
+
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`);
+  }
+}
+
+console.log("\n");
+
+///////////////////////////////////////////////////////////////////////
+
+const tabuada = 7;
+let i = 1;
+
+while (i <= 10) {
+  console.log(`${tabuada} x ${i} = ${tabuada * i}`);
+  i++;
+}
+
+console.log("\n");
+
+///////////////////////////////////////////////////////////////////////
+
+const alunos = [
+  { nome: "Paulo", nota: 7 },
+  { nome: "Marcela", nota: 9 },
+  { nome: "João", nota: 5 },
+  { nome: "Maicom", nota: 2 },
+];
+
+for (let aluno of alunos) {
+  if (aluno.nota < 7) {
+    continue;
+  }
+  console.log(`${aluno.nome} foi aprovado(a) com a nota = ${aluno.nota}`);
+}
+
+console.log("\n");
+
+///////////////////////////////////////////////////////////////////////
+
+const usuario = {
+  nome: "Guilherme",
+  amigos: ["Paulo", "Marcela", "João", "Maicom"],
+  idade: 22,
+  empregado: true,
+};
+
+for (let chave in usuario) {
+  console.log(`${chave} = ${usuario[chave]}`);
+}
+
+console.log("\n");
+
+///////////////////////////////////////////////////////////////////////
