c79732ba448526f025a39ba138c9d42ebbf4ab15
Henrique <henriquediniz.dev@gmail.com>
2026-09-15 15:34:27 -0300

    adiciona switch dias da semana

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+const numero = 3;
+let dia;
+
+switch (numero) {
+  case 1:
+    dia = "Domingo";
+    break;
+  case 2:
+    dia = "Segunda-feira";
+    break;
+  case 3:
+    dia = "Terça-feira";
+    break;
+  case 4:
+    dia = "Quarta-feira";
+    break;
+  case 5:
+    dia = "Quinta-feira";
+    break;
+  case 6:
+    dia = "Sexta-feira";
+    break;
+  case 7:
+    dia = "Sábado";
+    break;
+  default:
+    dia = "Número inválido";
+}
+
+console.log(dia);
