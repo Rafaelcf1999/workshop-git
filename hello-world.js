911509d3f072f4d70e0ebe312919c9bd964bad76
G-Deon <guilhermedeon29@gmail.com>
2026-09-15 15:31:53 -0300

    feat: implementação do switch

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+const dia = 0;
+
+switch (dia) {
+  case 1:
+    console.log("Domingo");
+    break;
+  case 2:
+    console.log("Segunda");
+    break;
+  case 3:
+    console.log("Terça");
+    break;
+  case 4:
+    console.log("Quarta");
+    break;
+  case 5:
+    console.log("Quinta");
+    break;
+  case 6:
+    console.log("Sexta");
+    break;
+  case 7:
+    console.log("Sábado");
+    break;
+  default:
+    console.log("Valor inválido! (somente 1-7 são válidos)");
+}
