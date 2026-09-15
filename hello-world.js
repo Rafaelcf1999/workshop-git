722fef3c60633c6b44af7e3906aafaa3e978fe8b
Thiago Markendorf <thima3101@gmail.com>
2026-09-15 15:37:10 -0300

    exercicio dias da semana

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,34 @@
-console.log("Hello World")
-console.log("Olá mundo")
+let n1 = Math.floor(Math.random() * 7) + 1;
 
+switch (n1) {
+    case 1:
+        console.log(n1)
+        console.log("Domingo")
+        break;
+    case 2:
+        console.log(n1)
+        console.log("Segunda")
+        break;
+    case 3:
+        console.log(n1)
+        console.log("Terça")
+        break;
+    case 4:
+        console.log(n1)
+        console.log("Quarta")
+        break;
+    case 5:
+        console.log(n1)
+        console.log("Quinta")
+        break;
+    case 6:
+        console.log(n1)
+        console.log("Sexta")
+        break;
+    case 7:
+        console.log(n1)
+        console.log("Sabado")
+        break;
+    default:
+        console.log("entrada invalida digite um numero entre 1 e 7")
+}
