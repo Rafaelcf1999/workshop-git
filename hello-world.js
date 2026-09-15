d403c28dda2559b534a75a2f72ee912b335175ec
Tarcisio Lucas <tarcisiolucas43@gmail.com>
2026-09-15 15:33:33 -0300

    feat: adiciona seleção dos dias da semana com switch case

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,34 @@
-console.log("Hello World")
+const dia = 1;
+
+switch (dia) {
+  case 1:
+    console.log("Domingo");
+    break;
+
+  case 2:
+    console.log("Segunda-feira");
+    break;
+
+  case 3:
+    console.log("Terça-feira");
+    break;
+
+  case 4:
+    console.log("Quarta-feira");
+    break;
+
+  case 5:
+    console.log("Quinta-feira");
+    break;
+
+  case 6:
+    console.log("Sexta-feira");
+    break;
+
+  case 7:
+    console.log("Sábado");
+    break;
+
+  default:
+    console.log("Opção inválida! Escolha um número de 1 a 7.");
+}
