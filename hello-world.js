0250192d7ecc8990a76630a176d4f6af5ab7e6b2
raianedantax <raianedantax@gmail.com>
2026-09-15 15:35:41 -0300

    feat: adiciona  o caso domingo

--- hello-world.js
+++ hello-world.js
@@ -2,21 +2,24 @@ const dia = 1;
 
 switch (dia) {
   case 1:
-    console.log('Segunda-feira');
+    console.log('Domingo');
     break;
   case 2:
-    console.log('Terça-feira');
+    console.log('Segunda-feira');
     break;
   case 3:
-    console.log('Quarta-feira');
+    console.log('Terça-feira');
     break;
   case 4:
-    console.log('Quinta-feira');
+    console.log('Quarta-feira');
     break;
   case 5:
-    console.log('Sexta-feira');
+    console.log('Quinta-feira');
     break;
   case 6:
+    console.log('Sexta-feira');
+    break;
+  case 7:
     console.log('Sábado');
     break;
   default:
