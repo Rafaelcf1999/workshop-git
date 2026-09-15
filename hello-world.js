7b9aec35dcf7a06a862a9eb4432c02823beae11f
raianedantax <raianedantax@gmail.com>
2026-09-15 15:31:10 -0300

    feat: switch case para dias da semana

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+const dia = 1;
+
+switch (dia) {
+  case 1:
+    console.log('Segunda-feira');
+    break;
+  case 2:
+    console.log('Terça-feira');
+    break;
+  case 3:
+    console.log('Quarta-feira');
+    break;
+  case 4:
+    console.log('Quinta-feira');
+    break;
+  case 5:
+    console.log('Sexta-feira');
+    break;
+  case 6:
+    console.log('Sábado');
+    break;
+  default:
+    console.log('Não há dia correspondente para o valor inserido.');
+}
