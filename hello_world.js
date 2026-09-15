b1c18832033bf8f50f52d11b0c7601b2adf5dd62
Stefanylima-dev <stefanycosta.contato@gmail.com>
2026-09-15 16:01:28 -0300

    Switch com  dias da semana

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,30 @@
-console.log("Hello World!")
+const diaDaSemana = 1;
+let nomeDoDia;
+
+switch (diaDaSemana) {
+  case 1:
+    nomeDoDia = "Domingo";
+    break;
+  case 2:
+    nomeDoDia = "Segunda";
+    break;
+  case 3:
+    nomeDoDia = "Terça";
+    break;
+  case 4:
+    nomeDoDia = "Quarta";
+    break;
+  case 5:
+    nomeDoDia = "Quinta";
+    break;
+  case 2:
+    nomeDoDia = "Sexta";
+    break;
+  case 2:
+    nomeDoDia = "Sabado";
+    break;
+  default:
+    nomeDoDia = "Dia inválido";
+}
+
+console.log(nomeDoDia);
