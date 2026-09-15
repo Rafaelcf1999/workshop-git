45be415aa7fa42759742c4e8c2f4e31ab301fa80
juliana15paak <fvjulianaasilva976@gmail.com>
2026-09-15 16:00:50 -0300

    feat: adiciona atividades do workshop 2 de switch e loop

---
+++ nota.js
@@ -0,0 +1,7 @@
+const notas = [0,1,2,3,4,5,6,7,8,9,10]
+for (let nota of notas){
+  if (nota < 7){
+    continue
+  }
+  console.log(`${nota}`)
+}
---
+++ tabuada.js
@@ -0,0 +1,13 @@
+const num = 10
+
+// com for
+for (let i = 1; i<=num; i++){
+  console.log(`${num} x ${i} = ${num * i}`)
+}
+
+// com while
+let i = 1;
+while (i <- num){
+  console.log(`${num} x ${i} = ${num * i}`)
+  i++
+}
