6880b287b08188476c34abe23571e986dbd97db4
Silas Soares <silasdesousa12345@gmail.com>
2026-09-15 15:28:02 -0300

    feat: verificação dos dias da semana com base no numero

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+//console.log("Hello World")
+
+let n = 1
+
+switch (n) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-feira");
+        break;
+    case 3:
+        console.log("Terça-feira");
+        break;
+    case 4:
+        console.log("Quarta-feira");
+        break;
+    case 5:
+        console.log("Quinta-feira");
+        break;
+    case 6:
+        console.log("Sexta-feira");
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("digite um número de 1 a 7");
+}
