f7626d7fcb9b37aaa3da54411ac228d8a00e7ed4
kevna2329 <kevna44@gmail.com>
2026-09-15 15:36:13 -0300

    feat : Implementação de switch case que exiba os dias da semana de acordo com um const dia que é declarada como int

--- hello-world.js
+++ hello-world.js
@@ -1,2 +1,27 @@
-console.log("Hello World")
+const dia = 3;
 
+switch (dia) {
+    case 1:
+        console.log("Hoje é domingo!");
+        break;
+    case 2:
+        console.log("Hoje é segunda-feira!");
+        break;
+    case 3:
+        console.log("Hoje é terça-feira!");
+        break;
+    case 4:
+        console.log("Hoje é quarta-feira!");
+        break;
+    case 5:
+        console.log("Hoje é quinta-feira!");
+        break;
+    case 6:
+        console.log("Hoje é sexta-feira!");
+        break;
+    case 7:
+        console.log("Hoje é sábado!");
+        break;
+    default:
+        console.log("Dia inválido");
+}
