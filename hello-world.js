8f2d2ba4d98c7b0930ca4b71d5026985f7a246e1
heduardowitkoski <heduardowitkoski@gmail.com>
2026-09-15 15:33:27 -0300

    Exercicio switch para dias da semana

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+let diaDaSemana = 1;
+
+switch (diaDaSemana) {
+  case 1:
+    console.log("Domingo");
+    break;
+  case 2:
+    console.log("Segunda-feira");
+    break;
+  case 3:
+    console.log("Terça-feira");
+    break;
+  case 4:
+    console.log("Quarta-feira");
+    break;
+  case 5:
+    console.log("Quinta-feira");
+    break;
+  case 6:
+    console.log("Sexta-feira");
+    break;
+  case 7:
+    console.log("Sábado");
+    break;
+  default:
+    console.log("Dia inválido");
+}
