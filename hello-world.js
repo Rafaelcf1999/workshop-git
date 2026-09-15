4c732094526f9ff0b6569de1ae41e8fc3e0a1884
KauaiP <kauaipereira@id.uff.br>
2026-09-15 15:32:38 -0300

    feat: implementando switch dos dias da semana em hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,26 @@
-console.log("Hello World")
+const dia = Math.random()*7 + 1
+
+switch (dia) {
+    case 1:
+        console.log("domingo")
+        break;
+    case 2:
+        console.log("segunda-feira")
+        break;
+    case 3:
+        console.log("terca-feira")
+        break;
+    case 4:
+        console.log("quarta-feira")
+        break;
+    case 5:
+        console.log("quinta-feira")
+        break;
+    case 6:
+        console.log("sexta-feira")
+        break;
+    default:
+        console.log("sabado")
+        break;
+}
+
