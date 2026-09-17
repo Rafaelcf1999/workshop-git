d1fd0299d21f1a90eb43a332dc6ea60ebaa6565f
raianedantax <raianedantax@gmail.com>
2026-09-17 16:00:20 -0300

    feat: desafios

--- hello-world.js
+++ hello-world.js
@@ -1,31 +1,45 @@
-console.log('Hello World');
-const numero = 3;
-let dia;
+const dia = 1;
 
-switch (numero) {
+switch (dia) {
   case 1:
-    dia = 'Domingo';
+    console.log('Segunda-feira');
     break;
   case 2:
-    dia = 'Segunda-feira';
+    console.log('Terça-feira');
     break;
   case 3:
-    dia = 'Terça-feira';
+    console.log('Quarta-feira');
     break;
   case 4:
-    dia = 'Quarta-feira';
+    console.log('Quinta-feira');
     break;
   case 5:
-    dia = 'Quinta-feira';
+    console.log('Sexta-feira');
     break;
   case 6:
-    dia = 'Sexta-feira';
-    break;
-  case 7:
-    dia = 'Sábado';
+    console.log('Sábado');
     break;
   default:
-    dia = 'Número inválido';
+    console.log('Não há dia correspondente para o valor inserido.');
 }
 
-console.log(dia);
+for (let i = 1; i <= 10; i++) {
+  console.log(`5 X ${i} = ${5 * i}`);
+}
+
+let tabuada = 7;
+let i = 1;
+
+while (i <= 10) {
+  console.log(`${tabuada} X ${i} = ${tabuada * i}`);
+  i++;
+}
+
+const notas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let nota of notas) {
+  if (nota < 7) {
+    continue;
+  }
+  console.log(`${nota}`);
+}
