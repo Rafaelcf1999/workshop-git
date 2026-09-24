e78b790d00758978230ae758a3f26d93d759ce78
Leandro Silva <leandropenha76@gmail.com>
2026-09-24 18:37:22 -0300

    feat: cria a estrutura principal e as entidades

---
+++ src/entities/Book.ts
@@ -0,0 +1,24 @@
+export class Book {
+  constructor(
+    public readonly id: number,
+    public readonly title: string,
+    public readonly author: string,
+    public readonly category: string,
+    private quantity: number
+  ){}
+
+  public decrease(): void {
+    if (this.quantity <= 0) {
+      throw new Error('No copies available');
+    }
+    this.quantity--;
+  }
+
+  public increase(): void {
+    this.quantity++;
+  }
+
+  public getQuantity(): number {
+    return this.quantity;
+  }
+}
