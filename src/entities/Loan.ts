818c7cd7c567c4139f543df025e52974e0dfbee1
Stéfany Lima <stefanycosta.contato@gmail.com>
2026-09-25 17:21:19 -0300

    feat: implementacao das entidades (Book, User e Loan)

---
+++ src/entities/Loan.ts
@@ -0,0 +1,9 @@
+export class Loan{
+    readonly userId: number;
+    readonly bookId: number;
+
+    constructor(userId: number, bookId: number){
+        this.userId = userId;
+        this.bookId = bookId;
+    } 
+}
