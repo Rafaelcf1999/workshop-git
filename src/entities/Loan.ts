51209ad9bead137dc11537b40c9eb1228b1517b0
T-lucas43 <tarcisiolucas43@gmail.com>
2026-09-26 11:14:52 -0300

    feat: adiciona entidade de empréstimo

---
+++ src/entities/Loan.ts
@@ -0,0 +1,9 @@
+export class Loan {
+  public readonly userId: number;
+  public readonly bookId: number;
+
+  constructor(userId: number, bookId: number) {
+    this.userId = userId;
+    this.bookId = bookId;
+  }
+}
