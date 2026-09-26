cef95e8065d9eaf7b8f405258757b749d221492d
T-lucas43 <tarcisiolucas43@gmail.com>
2026-09-26 10:42:24 -0300

    feat: adiciona entidade de livro

---
+++ src/entities/Book.ts
@@ -0,0 +1,37 @@
+export class Book {
+  public id: number;
+  public title: string;
+  public author: string;
+  public category: string;
+  private quantity: number;
+
+  constructor(
+    id: number,
+    title: string,
+    author: string,
+    category: string,
+    quantity: number,
+  ) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.category = category;
+    this.quantity = quantity;
+  }
+
+  public decrease(): void {
+    if (this.quantity <= 0) {
+      throw new Error("No copies available");
+    }
+
+    this.quantity -= 1;
+  }
+
+  public increase(): void {
+    this.quantity += 1;
+  }
+
+  public getQuantity(): number {
+    return this.quantity;
+  }
+}
