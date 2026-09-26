e9c90b966f3824b0fef8c69a247cf3b3a3c9d618
kevna2329 <kevna44@gmail.com>
2026-09-26 10:59:27 -0300

    test: manually verify UserRepository save and duplicate error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { BookRepository } from "./repositories/BookRepository.ts";
 import { Book } from "./entities/Book.ts";
+import { UserRepository } from "./repositories/UserRepository.ts";
+import { User } from "./entities/User.ts";
 
+//test BookRepository
 const testeRepo = new BookRepository();
 const testeBook = new Book(99, "teste", "autor", "categoria", 1);
 
@@ -9,5 +12,16 @@ console.log(testeRepo.findById(99));
 try{
     testeRepo.save(testeBook);
 }catch(e){
+    console.log((e as Error).message);
+}
+
+//test UserRepository
+const testUserRepo = new UserRepository();
+const testUser = new User(99, "Usuário Teste");
+testUserRepo.save(testUser);
+console.log(testUserRepo.findById(99));
+try {
+    testUserRepo.save(testUser);
+} catch (e) {
     console.log((e as Error).message);
 }
